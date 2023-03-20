887faff2d5270dbd5c082f824e7efbd39ed8ca0b
RevEmmanuel <adeolaae1@gmail.com>
2023-03-20 10:12:06 +0100

    Class list view

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello/", views.hello, name='hello'),
-    path("greet/", views.greet, name='greet'),
-    path("simi/", views.simi, name='simi'),
-    path("", views.home, name='home'),
+    path('hello/', views.hello, name='hello'),
+    path('greet/', views.greet, name='greet'),
+    path('simi/', views.simi, name='simi'),
+    # path('', views.home, name='home'),
     path('detail/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('', views.PostListView.as_view(), name='home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
 ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from blogApp.models import Post
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 
 # Create your views here.
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blogApp/post_detail.html', {'post': post})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogApp/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogApp/post_detail.html'
+    context_object_name = 'post'
 
 
 def home(request):
