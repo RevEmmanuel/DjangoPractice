77c1149576ddf9a7f5eb5d186f4f48de2f01e9df
RevEmmanuel <adeolaae1@gmail.com>
2023-03-21 08:02:53 +0100

    Tried reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('reset/', views.ResetPasswordView.as_view(), name='reset'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django.urls import reverse_lazy
 
 
@@ -9,3 +9,9 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+class ResetPasswordView(generic.edit.FormView):
+    form_class = PasswordResetForm
+    success_url = reverse_lazy('password_reset_done')
+    template_name = 'accounts/reset_password.html'
